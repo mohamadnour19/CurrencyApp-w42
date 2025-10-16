9ba95bfeb5b23f524f0aeafc1769ad782d74346d
Mohamad Nour Shich Khalil <mohmad200.mm16@gmail.com>
2025-10-16 21:07:59 +0200

    fixed strucure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting Currency Converter App");
+
+        Menu menu = new Menu();
+        menu.run();
+        
+    }
+}
